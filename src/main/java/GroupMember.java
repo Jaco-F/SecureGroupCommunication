21357ab177a35758f617b41a2491c10f39c0b461
AmedeoAsnaghi <amedeo.asnaghi@gmail.com>
2015-03-19 15:49:21 +0000

    Improved member

--- src/main/java/GroupMember.java
+++ src/main/java/GroupMember.java
@@ -18,6 +18,7 @@ public class GroupMember implements Runnable{
 
     private MulticastSocket listeningSocket;
     private Socket serverSocket;
+    private ServerSocket clientSocket;
     private int port;
 
     private Cipher desCipher;
@@ -25,13 +26,15 @@ public class GroupMember implements Runnable{
 
     private final String serverAddress = "192.168.0.2";
     private final int SERVER_PORT = 13000;
-
+    private final int CLIENT_PORT = 14000;
     private final int MONITORING_PORT = 15000;
     private  String hostName ="";
 
     private String groupAddress = "239.0.0.1";
 
     private Thread monitoringThread;
+
+    private boolean joined;
 
     public GroupMember(int port) {
         this.port = port;
@@ -68,7 +71,17 @@ public class GroupMember implements Runnable{
 
     @Override
     public void run() {
-        join();
+        //join();
+        joined = false;
+        try {
+            listeningSocket = new MulticastSocket(port);
+            InetAddress address = InetAddress.getByName(groupAddress);
+            hostName = Inet4Address.getLocalHost().getHostName();
+            listeningSocket.joinGroup(address);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         Thread writeThread = new Thread(new WriteChat(this));
         writeThread.start();
 
@@ -87,106 +100,115 @@ public class GroupMember implements Runnable{
                 objectInputStream = new ObjectInputStream(bis);
                 inputMsg = (Message)objectInputStream.readObject();
 
-                if(inputMsg instanceof ServerJoinKeys){
-                    System.out.println("Another client is in the group");
-                    ServerJoinKeys serverJoinKeys = (ServerJoinKeys) inputMsg;
+                if (joined) {
+                    if (inputMsg instanceof ServerJoinKeys) {
+                        System.out.println("Another client is in the group");
+                        ServerJoinKeys serverJoinKeys = (ServerJoinKeys) inputMsg;
 
-                    //DECRYPT DEK
-                    byte[] decDek;
-                    try {
-                        desCipher.init(Cipher.DECRYPT_MODE,dek);
-                        decDek = desCipher.doFinal(serverJoinKeys.getDek());
-                        dek = new SecretKeySpec(decDek,0,decDek.length,"DES");
-                    } catch (BadPaddingException e) {
-                        System.out.println("Error in dek decryption");
-                    }
-
-                    //DECRYPT KEK1
-                    byte[] decKek;
-                    try {
-                        desCipher.init(Cipher.DECRYPT_MODE,keks[0]);
-                        decKek = desCipher.doFinal(serverJoinKeys.getKek1());
-                        keks[0] = new SecretKeySpec(decKek,0,decKek.length,"DES");
-                    } catch (BadPaddingException e) {
-                        System.out.println("I don't need the kek 1");
-                    }
-
-                    //DECRYPT KEK2
-                    try {
-                        desCipher.init(Cipher.DECRYPT_MODE,keks[1]);
-                        decKek = desCipher.doFinal(serverJoinKeys.getKek2());
-                        keks[1] = new SecretKeySpec(decKek,0,decKek.length,"DES");
-                    } catch (BadPaddingException e) {
-                        System.out.println("I don't need the kek 2");
-                    }
-
-                    //DECRYPT KEK3
-                    try {
-                        desCipher.init(Cipher.DECRYPT_MODE,keks[2]);
-                        decKek = desCipher.doFinal(serverJoinKeys.getKek3());
-                        keks[2] = new SecretKeySpec(decKek,0,decKek.length,"DES");
-                    } catch (BadPaddingException e) {
-                        System.out.println("I don't need the kek 3");
-                    }
-
-                }else if (inputMsg instanceof TextMessage){
-                    TextMessage txMessage = (TextMessage) inputMsg;
-                    desCipher.init(Cipher.DECRYPT_MODE,dek);
-                    try {
-                        byte [] decMsg = desCipher.doFinal(txMessage.getText());
-                        String msg = new String(decMsg);
-                        System.out.println(txMessage.getHostName() + " : " + msg);
-                    } catch (BadPaddingException e) {
-                        System.out.println("Received a message I couldn't decrypt");
-                    }
-                }else if (inputMsg instanceof ServerLeaveDek) {
-                    ServerLeaveDek serverLeaveDek = (ServerLeaveDek) inputMsg;
-                    byte[] decDek;
-
-                    //I NEED TO TRY WITH ALL MY KEKS IF I CAN DECRYPT THE MESSAGE
-                    //THIS WAY I CAN OBTAIN THE NEW DEK
-                    for (SecretKey kek : keks) {
+                        //DECRYPT DEK
+                        byte[] decDek;
                         try {
-                            desCipher.init(Cipher.DECRYPT_MODE,kek);
-                            decDek = desCipher.doFinal(serverLeaveDek.getDek());
-                            dek = new SecretKeySpec(decDek,0,decDek.length,"DES");
+                            desCipher.init(Cipher.DECRYPT_MODE, dek);
+                            decDek = desCipher.doFinal(serverJoinKeys.getDek());
+                            dek = new SecretKeySpec(decDek, 0, decDek.length, "DES");
                         } catch (BadPaddingException e) {
-                            //System.out.println("");
+                            System.out.println("Error in dek decryption");
+                        }
+
+                        //DECRYPT KEK1
+                        byte[] decKek;
+                        try {
+                            desCipher.init(Cipher.DECRYPT_MODE, keks[0]);
+                            decKek = desCipher.doFinal(serverJoinKeys.getKek1());
+                            keks[0] = new SecretKeySpec(decKek, 0, decKek.length, "DES");
+                        } catch (BadPaddingException e) {
+                            System.out.println("I don't need the kek 1");
+                        }
+
+                        //DECRYPT KEK2
+                        try {
+                            desCipher.init(Cipher.DECRYPT_MODE, keks[1]);
+                            decKek = desCipher.doFinal(serverJoinKeys.getKek2());
+                            keks[1] = new SecretKeySpec(decKek, 0, decKek.length, "DES");
+                        } catch (BadPaddingException e) {
+                            System.out.println("I don't need the kek 2");
+                        }
+
+                        //DECRYPT KEK3
+                        try {
+                            desCipher.init(Cipher.DECRYPT_MODE, keks[2]);
+                            decKek = desCipher.doFinal(serverJoinKeys.getKek3());
+                            keks[2] = new SecretKeySpec(decKek, 0, decKek.length, "DES");
+                        } catch (BadPaddingException e) {
+                            System.out.println("I don't need the kek 3");
+                        }
+
+                    } else if (inputMsg instanceof TextMessage) {
+                        TextMessage txMessage = (TextMessage) inputMsg;
+                        desCipher.init(Cipher.DECRYPT_MODE, dek);
+                        try {
+                            byte[] decMsg = desCipher.doFinal(txMessage.getText());
+                            String msg = new String(decMsg);
+                            System.out.println(txMessage.getHostName() + " : " + msg);
+                        } catch (BadPaddingException e) {
+                            System.out.println("Received a message I couldn't decrypt");
+                        }
+
+                    } else if (inputMsg instanceof ServerLeaveDek) {
+                        ServerLeaveDek serverLeaveDek = (ServerLeaveDek) inputMsg;
+                        byte[] decDek;
+
+                        //I NEED TO TRY WITH ALL MY KEKS IF I CAN DECRYPT THE MESSAGE
+                        //THIS WAY I CAN OBTAIN THE NEW DEK
+                        for (SecretKey kek : keks) {
+                            try {
+                                desCipher.init(Cipher.DECRYPT_MODE, kek);
+                                decDek = desCipher.doFinal(serverLeaveDek.getDek());
+                                dek = new SecretKeySpec(decDek, 0, decDek.length, "DES");
+                            } catch (BadPaddingException e) {
+                                //System.out.println("");
+                            }
+                        }
+                    } else if (inputMsg instanceof ServerLeaveKek) {
+                        ServerLeaveKek serverLeaveKek = (ServerLeaveKek) inputMsg;
+                        byte[] firstPhaseDecriptedKek;
+                        byte[] decKek;
+                        try {
+                            desCipher.init(Cipher.DECRYPT_MODE, dek);
+                            firstPhaseDecriptedKek = desCipher.doFinal(serverLeaveKek.getKek1());
+                            desCipher.init(Cipher.DECRYPT_MODE, keks[0]);
+                            decKek = desCipher.doFinal(firstPhaseDecriptedKek);
+                            keks[0] = new SecretKeySpec(decKek, 0, decKek.length, "DES");
+                        } catch (BadPaddingException e) {
+                            System.out.println("I don't need kek 1");
+                        }
+                        try {
+                            desCipher.init(Cipher.DECRYPT_MODE, dek);
+                            firstPhaseDecriptedKek = desCipher.doFinal(serverLeaveKek.getKek2());
+                            desCipher.init(Cipher.DECRYPT_MODE, keks[1]);
+                            decKek = desCipher.doFinal(firstPhaseDecriptedKek);
+                            keks[1] = new SecretKeySpec(decKek, 0, decKek.length, "DES");
+                        } catch (BadPaddingException e) {
+                            System.out.println("I don't need kek 2");
+                        }
+                        try {
+                            desCipher.init(Cipher.DECRYPT_MODE, dek);
+                            firstPhaseDecriptedKek = desCipher.doFinal(serverLeaveKek.getKek3());
+                            desCipher.init(Cipher.DECRYPT_MODE, keks[2]);
+                            decKek = desCipher.doFinal(firstPhaseDecriptedKek);
+                            keks[2] = new SecretKeySpec(decKek, 0, decKek.length, "DES");
+                        } catch (BadPaddingException e) {
+                            System.out.println("I don't need kek 3");
                         }
                     }
-                } else if (inputMsg instanceof ServerLeaveKek) {
-                    ServerLeaveKek serverLeaveKek = (ServerLeaveKek) inputMsg;
-                    byte[] firstPhaseDecriptedKek;
-                    byte[] decKek;
-                    try {
-                        desCipher.init(Cipher.DECRYPT_MODE,dek);
-                        firstPhaseDecriptedKek = desCipher.doFinal(serverLeaveKek.getKek1());
-                        desCipher.init(Cipher.DECRYPT_MODE,keks[0]);
-                        decKek = desCipher.doFinal(firstPhaseDecriptedKek);
-                        keks[0] = new SecretKeySpec(decKek,0,decKek.length,"DES");
-                    } catch (BadPaddingException e) {
-                        System.out.println("I don't need kek 1");
-                    }
-                    try {
-                        desCipher.init(Cipher.DECRYPT_MODE,dek);
-                        firstPhaseDecriptedKek = desCipher.doFinal(serverLeaveKek.getKek2());
-                        desCipher.init(Cipher.DECRYPT_MODE,keks[1]);
-                        decKek = desCipher.doFinal(firstPhaseDecriptedKek);
-                        keks[1] = new SecretKeySpec(decKek,0,decKek.length,"DES");
-                    } catch (BadPaddingException e) {
-                        System.out.println("I don't need kek 2");
-                    }
-                    try {
-                        desCipher.init(Cipher.DECRYPT_MODE,dek);
-                        firstPhaseDecriptedKek = desCipher.doFinal(serverLeaveKek.getKek3());
-                        desCipher.init(Cipher.DECRYPT_MODE,keks[2]);
-                        decKek = desCipher.doFinal(firstPhaseDecriptedKek);
-                        keks[2] = new SecretKeySpec(decKek,0,decKek.length,"DES");
-                    } catch (BadPaddingException e) {
-                        System.out.println("I don't need kek 3");
+                }
+                else {
+                    if (inputMsg instanceof TextMessage){
+                        TextMessage txMessage = (TextMessage) inputMsg;
+                        String msg = new String(txMessage.getText());
+                        System.out.println(txMessage.getHostName() + " : " + msg);
                     }
                 }
-
             } catch (IOException | ClassNotFoundException | IllegalBlockSizeException | InvalidKeyException e) {
                 e.printStackTrace();
             }
@@ -195,6 +217,10 @@ public class GroupMember implements Runnable{
     }
 
     private void join(){
+        //------------------------------------------------------
+        //INIT SOCKETs AND CIPHERs
+        //------------------------------------------------------
+
         Message message = new JoinMessage(publicKey);
         ObjectOutputStream objectOutputStream = null;
         ObjectInputStream objectInputStream = null;
@@ -204,15 +230,17 @@ public class GroupMember implements Runnable{
 
 
         try {
-            //Prepare to join multicast group
-            listeningSocket = new MulticastSocket(port);
-
-            InetAddress address = InetAddress.getByName(groupAddress);
-
-            hostName = Inet4Address.getLocalHost().getHostName();
-            listeningSocket.joinGroup(address);
+            //Sockets init
+            clientSocket = new ServerSocket(CLIENT_PORT);
+            serverSocket = new Socket(serverAddress,SERVER_PORT);
 
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            rsaCipher.init(Cipher.DECRYPT_MODE,privateKey);
+        } catch (InvalidKeyException e) {
             e.printStackTrace();
         }
 
@@ -220,12 +248,6 @@ public class GroupMember implements Runnable{
         //------------------------------------------------------
         //CONNECT TO THE SERVER AND SEND HIM THE JOIN MESSAGE
         //------------------------------------------------------
-
-        try {
-            serverSocket = new Socket(serverAddress, SERVER_PORT);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         try {
             objectOutputStream = new ObjectOutputStream(serverSocket.getOutputStream());
@@ -246,15 +268,11 @@ public class GroupMember implements Runnable{
         //WAIT FOR THE SERVER RESPONSE
         //-----------------------------------------------------
 
+        Socket server = null;
         try {
+            server = clientSocket.accept();
+            objectInputStream = new ObjectInputStream(server.getInputStream());
 
-            byte[] b = new byte[65535];
-            ByteArrayInputStream bis = new ByteArrayInputStream(b);
-            DatagramPacket dgram = new DatagramPacket(b, b.length);
-
-            listeningSocket.receive(dgram);
-
-            objectInputStream = new ObjectInputStream(bis);
             initMessage = (ServerJoinKeys)objectInputStream.readObject();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
@@ -262,11 +280,7 @@ public class GroupMember implements Runnable{
 
 
         System.out.println("Message with keks and dek Received");
-        try {
-            rsaCipher.init(Cipher.DECRYPT_MODE,privateKey);
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        }
+
 
         //DECRYPTS AND SAVE DEK
         try {
@@ -300,23 +314,27 @@ public class GroupMember implements Runnable{
         }
 
         monitoringThread.start();
-
+        joined = true;
     }
 
     private void broadcastMessage(String msg) {
         ObjectOutputStream objectOutputStream = null;
         TextMessage txMessage = new TextMessage();
+
         try {
-
-            try {
-                desCipher.init(Cipher.ENCRYPT_MODE,dek);
-                byte[] msgEnc = desCipher.doFinal(msg.getBytes());
-                txMessage.setText(msgEnc);
-                txMessage.setHostName(hostName);
-            } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
-                e.printStackTrace();
+            txMessage.setHostName(hostName);
+            if(!joined){
+                txMessage.setText(msg.getBytes());
             }
-
+            else {
+                try {
+                    desCipher.init(Cipher.ENCRYPT_MODE, dek);
+                    byte[] msgEnc = desCipher.doFinal(msg.getBytes());
+                    txMessage.setText(msgEnc);
+                } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+                    e.printStackTrace();
+                }
+            }
             ByteArrayOutputStream b_out = new ByteArrayOutputStream();
             try {
                 objectOutputStream = new ObjectOutputStream(b_out);
@@ -364,6 +382,7 @@ public class GroupMember implements Runnable{
         }
 
         monitoringThread.interrupt();
+        joined = false;
     }
 
 
@@ -379,6 +398,7 @@ public class GroupMember implements Runnable{
         public void run() {
             //LISTEN FOR KEYBOARD INPUT
             String line="";
+            System.out.println("Welcome! Type the message to broadcast to the multicast group. :join to join or :leave to leave)");
             while(true){
 
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -388,8 +408,11 @@ public class GroupMember implements Runnable{
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                if(line.equals("exit")){
+                if(line.equals(":leave")){
                     groupMember.sendLeaveMsg();
+                }
+                else if(line.equals(":join")){
+                    groupMember.join();
                 }
                 else{
                     groupMember.broadcastMessage(line);
